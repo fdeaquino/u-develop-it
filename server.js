65f2ff7ee27a2c17ebaf5fd00c8eb2ddcd4c2d06
FDeaquino <fidel.deaquino@gmail.com>
2022-10-23 15:21:27 -0500

    built database calls

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Saguaro2021!', 
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// IMPORTANT: this method is the key component that allows SQL commands to be written in a Node.js applicaiton 
+// Breakdown: The db object is using the SQL query() method which executes the callback with the resulting rows that match the query - More explanation in 12.2.4
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
-// route handles user requests that aren't supported by the app
+// Catchall route handles user requests that aren't supported by the app
 app.use((req, res) => {
     res.status(404).end();
 });
