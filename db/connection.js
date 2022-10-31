ff4238b0d9546ebb9c60c47c6c01a634f6bf851b
FDeaquino <fidel.deaquino@gmail.com>
2022-10-31 10:24:16 -0500

    mysql edit

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'Saguaro2021!',
+        password: 'Hello123!',
         database: 'election'
     },
     console.log('Connected to the election database.')
